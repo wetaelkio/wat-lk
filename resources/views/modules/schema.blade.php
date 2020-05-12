2df0455fc2468cb7682c1a0c1ab0f06f70f56d99
Emmanuel Joseph(JET) <emmanueljet774@gmail.com>
2020-05-12 02:51:39 +0100

    Update schema.blade.php

--- resources/views/modules/schema.blade.php
+++ resources/views/modules/schema.blade.php
@@ -6,14 +6,14 @@
     "name" : "@yield('pageTitle', config('app.name'))",
     "alternateName" : "{{ config('app.name') }}",
     "email" : "info.wetaelk@gmail.com",
-    "image" : "{{ config('app.url') }}/assets/images/brand/logo-wbg.png",
+    "image" : "{{ config('app.url') }}/assets/images/brand/logo-wbg.jpg",
     "description" : "@yield('pageDescription', 'WeTælk is a social media that is passionate about your idea, ready to promoting love and equitable distribution of wealth')",
     "url" : "{{ url()->current() }}",
     "logo": {
       "@type": "ImageObject",
       "text": "{{ config('app.name') }}",
       "url": "{{ config('app.url') }}/assets/images/brand/logo-blue.png",
-      "sameAs" : "{{ config('app.url') }}/assets/images/brand/logo-wbg.png",
+      "sameAs" : "{{ config('app.url') }}/assets/images/brand/logo-wbg.jpg",
       "about": "{{ config('app.name') }} logo"
     }
   }
